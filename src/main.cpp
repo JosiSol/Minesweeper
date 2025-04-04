fab0cdf45855da26f2f64a3899510e83f34957cb
Yosef Solomon <yosefsolomon21@gmail.com>
2025-04-04 15:53:15 +0300

    Flood Fill and Flag Implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <raylib.h>
 #include <raymath.h>
 
-#include <iostream>
+#include <vector>
+#include <utility>
+#include <random>
 
+// #include <iostream>
+
+// ROWS AND COLS OF THE GRID
 #define ROWS 10
 #define COLS 10
 
@@ -12,11 +17,15 @@ const int SCREENHEIGHT = 400;
 const int CELLWIDTH = SCREENWIDTH / COLS;
 const int CELLHEIGHT = SCREENHEIGHT / ROWS;
 
+Texture2D flagSprite;
+
 typedef struct Cell{
     int i;
     int j;
     bool containsMine;
     bool isRevealed;
+    bool isFlagged;
+    int neighborMines;
 } Cell;
 
 Cell grid[ROWS][COLS];
@@ -24,11 +33,15 @@ Cell grid[ROWS][COLS];
 void CellDraw(Cell cell);
 void CellReveal(int i, int j);
 bool InBound(int i, int j);
+int NeighborMines(int i, int j);
+void CellFlag(int i, int j);
 
 int main()
 {
     InitWindow(SCREENWIDTH, SCREENHEIGHT, "Minesweeper");
     SetTargetFPS(60);
+
+    flagSprite = LoadTexture("Assets/flag.png");
 
     // Initialize grid
     for (int i = 0; i < ROWS; i++){
@@ -37,6 +50,38 @@ int main()
             grid[i][j].j = j;
             grid[i][j].containsMine = false;
             grid[i][j].isRevealed = false;
+            grid[i][j].isFlagged = false;
+            grid[i][j].neighborMines = -1;
+
+        /* if (i == 0){
+            grid[i][j].containsMine = true; // Testing, Set First Column to Mine ✅
+            } */
+        }
+    }
+    int mines = (ROWS * COLS) / 10; // 10% of the grid
+
+    std::vector<std::pair<int, int>> minePositions;
+    for (int i = 0; i < ROWS; i++){
+        for (int j = 0; j < COLS; j++){
+            minePositions.push_back({i, j});
+        }
+    }
+    
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::shuffle(minePositions.begin(), minePositions.end(), gen);
+    
+    for (int p = 0; p < mines; p++){
+        int i = minePositions[p].first;
+        int j = minePositions[p].second;
+        grid[i][j].containsMine = true;
+    }
+
+    for (int i = 0; i < ROWS; i++){
+        for (int j = 0; j < COLS; j++){
+            if (!grid[i][j].containsMine){
+                grid[i][j].neighborMines = NeighborMines(i, j);
+            } 
         }
     }
 
@@ -50,6 +95,14 @@ int main()
 
             if(InBound(i, j)){
                 CellReveal(i, j);
+            }
+        } else if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)){
+            Vector2 mousePos = GetMousePosition();
+            int i = mousePos.x / CELLWIDTH;
+            int j = mousePos.y / CELLHEIGHT;
+
+            if(InBound(i, j)){
+                CellFlag(i, j);
             }
         }
 
@@ -71,16 +124,28 @@ int main()
 
     return 0;
 }
+
 void CellDraw(Cell cell){
     if (cell.isRevealed){
         if (cell.containsMine){
             DrawRectangle(cell.i * CELLWIDTH, cell.j * CELLHEIGHT, CELLWIDTH, CELLHEIGHT, RED); 
         } else {
             DrawRectangle(cell.i * CELLWIDTH, cell.j * CELLHEIGHT, CELLWIDTH, CELLHEIGHT, LIGHTGRAY); 
+            if (cell.neighborMines){
+            DrawText(TextFormat("%d", cell.neighborMines), cell.i * CELLWIDTH + CELLWIDTH / 4, cell.j * CELLHEIGHT + CELLHEIGHT / 4, 20, DARKGRAY);
+            }
         }
+    } else if (cell.isFlagged){
+        Rectangle src = {0, 0, static_cast<float>(flagSprite.width), static_cast<float>(flagSprite.height)};
+        Rectangle dest = {
+            static_cast<float>(cell.i * CELLWIDTH), static_cast<float>(cell.j * CELLHEIGHT), static_cast<float>(CELLWIDTH), static_cast<float>(CELLHEIGHT)
+        };
+        Vector2 origin = {0, 0};
+        DrawTexturePro(flagSprite, src, dest, origin, 0.0f, LIGHTGRAY);
     }
     DrawRectangleLines(cell.i * CELLWIDTH, cell.j * CELLHEIGHT, CELLWIDTH, CELLHEIGHT, BLACK); 
 }
+
 bool InBound(int i, int j) {
     return i >= 0 && i < ROWS && j >= 0 && j < COLS;
 }
@@ -89,11 +154,58 @@ void CellReveal(int i, int j){
     if (!InBound(i, j)){
         return;
     }
+    if (grid[i][j].isFlagged || grid[i][j].isRevealed){
+        return;
+    }
+
     grid[i][j].isRevealed = true;
-    std::cout << "Cell (" << i << ", " << j << ") revealed." << std::endl;
+    // std::cout << "Cell (" << i << ", " << j << ") revealed." << std::endl;
     if (grid[i][j].containsMine){
-        // Game Over
+        // Game Over Logic
     } else{
         // Play Sound and Continue Logic
+
+        if (grid[i][j].neighborMines == 0){
+            // Flood Fill Logic
+            for (int x = -1; x <= 1; x++){
+                for (int y = -1; y <= 1; y++){
+                    if (x == 0 && y == 0){
+                        continue;
+                    }
+                    if (InBound(i + x, j + y)){
+                        if (!grid[i + x][j + y].isRevealed && !grid[i + x][j + y].containsMine){
+                            CellReveal(i + x, j + y);
+                        }   
+                    }
+                }
+            }
+        }
+    }
+}
+
+int NeighborMines(int i, int j){
+    int count = 0;
+    for (int x = -1; x <= 1; x++){
+        for (int y = -1; y <= 1; y++){
+            if (x == 0 && y == 0){
+                continue;
+            }
+            if (InBound(i + x, j + y)){
+                if (grid[i + x][j + y].containsMine){
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
+}
+
+void CellFlag(int i, int j){
+    if (grid[i][j].isRevealed){
+        return;
+    }
+
+    if (InBound(i, j)){
+        grid[i][j].isFlagged = !grid[i][j].isFlagged;
     }
 }
